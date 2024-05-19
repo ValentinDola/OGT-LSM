87048c4c33e618135505ddb2b2d9ee5ff1593ecf
Valentino Dola <dolavalentino@gmail.com>
2024-05-19 19:07:38 +0000

    uploathing core.ts

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -12,10 +12,10 @@ const handleAuth = () => {
  
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
-    courseImage: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
+    courseImage: f({ image: { maxFileSize: "8MB", maxFileCount: 1 } })
         .middleware(() => handleAuth())
         .onUploadComplete(() => {}),
-    courseAttachement: f(["text", "image", "video", "audio", "pdf"])
+    courseAttachment: f(["text", "image", "video", "audio", "pdf"] )
         .middleware(() => handleAuth())
         .onUploadComplete(() => { }),
     
