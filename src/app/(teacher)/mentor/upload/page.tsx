0ba6927f3e0075e91023672e10ea4f9bade17a92
Valentino Dola <dolavalentino@gmail.com>
2024-05-23 23:12:42 +0000

    Upload ( PAGE, COLUMNS, DATA-TABLE )

--- src/app/(teacher)/mentor/upload/page.tsx
+++ src/app/(teacher)/mentor/upload/page.tsx
@@ -1,12 +1,28 @@
-"use client";
-
 import { Button } from "@/components/ui/button";
 import { useUser } from "@clerk/nextjs";
-import { useRouter } from "next/navigation";
+import { redirect, useRouter } from "next/navigation";
+import { Payment, columns } from "./_components/columns";
+import { DataTable } from "./_components/data-table";
+import { auth, currentUser } from "@clerk/nextjs/server";
+import { db } from "@/lib/db";
 
-export default function Uplaod() {
-  const router = useRouter();
-  const { user, isLoaded, isSignedIn } = useUser();
+export default async function Uplaod() {
+  const user = await currentUser();
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const courses = await db.course.findMany({
+    where: {
+      userId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
   return (
     <section className="max-w-[1380px] w-full mx-auto overflow-hidden  mt-[100px] mb-10">
       <div className="my-0 px-5 py-0">
@@ -14,6 +30,9 @@ export default function Uplaod() {
           <h2 className="text-[3rem] tracking-[-0.064em] font-semibold max-[986px]:text-[3rem] max-[400px]:text-[2rem] max-[600px]:text-[2.3rem]">
             Howdy, Mentor {user?.username}
           </h2>
+        </div>
+        <div className="container mx-auto py-10">
+          <DataTable columns={columns} data={courses} />
         </div>
       </div>
     </section>
