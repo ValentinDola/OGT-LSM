33147609edc58e5a8cfbffccf7532b5002728f4b
Valentino Dola <dolavalentino@gmail.com>
2024-06-05 20:35:45 +0000

    updated subs courses

--- src/app/(marketing)/(routes)/dashboard/_components/subscription-courses.tsx
+++ src/app/(marketing)/(routes)/dashboard/_components/subscription-courses.tsx
@@ -10,7 +10,7 @@ import { useRouter } from "next/navigation";
 type CourseWithProgressWithCategory = Course & {
   category: Category | null;
   chapters: { id: string }[];
-  progress: number | null;
+  progress: number | undefined;
 };
 
 interface SubscriptionCoursesProps {
